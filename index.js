105aeae9e30de01236974fd86750322023c8ec9f
Arfan1317 <piasharfanulhoque@gmail.com>
2026-07-26 10:26:42 +0600

    added root route and vercel module exports

--- index.js
+++ index.js
@@ -6,13 +6,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
-app.use(express.json()); 
-
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@skillspherecluster.ccixorm.mongodb.net/?appName=SkillSphereCluster`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,22 +21,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+   
     await client.connect();
     
-   
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-    
+   
     const db = client.db("docAppointDB");
     const usersCollection = db.collection("users");
     const appointmentsCollection = db.collection("appointments");
-
-   
-    app.get('/', (req, res) => {
-      res.send('DocAppoint Server is running smoothly!');
-    });
 
   } finally {
     
@@ -48,7 +40,13 @@ async function run() {
 run().catch(console.dir);
 
 
+app.get('/', (req, res) => {
+  res.send('DocAppoint Server is running smoothly!');
+});
+
 app.listen(port, () => {
   console.log(`DocAppoint Server is listening on port ${port}`);
 });
+
+
 module.exports = app;
